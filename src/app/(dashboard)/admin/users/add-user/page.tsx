97747d29874e161bd2ec1208f2883e76d42af4de
Prashantha K.G.M <it21169908@my.sliit.lk>
2024-11-29 02:50:04 +0530

    Add changes

---
+++ src/app/(dashboard)/admin/users/add-user/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import {NextPage} from "next";
+import React from "react";
+
+const AddUsersPage: NextPage = () => {
+
+    return (
+        <>
+            <div className="intro-y mt-8 flex items-center">
+                <h2 className="mr-auto text-lg font-medium">Add New User</h2>
+            </div>
+        </>
+    );
+};
+
+export default AddUsersPage;
