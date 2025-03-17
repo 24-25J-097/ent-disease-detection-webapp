bcc011436eba29ee92ef7174c069cae4313048ac
Prashantha K.G.M <it21169908@my.sliit.lk>
2025-03-17 15:14:34 +0530

    Add packages and relevant types

---
+++ src/types/Charts.d.ts
@@ -0,0 +1,19 @@
+export type DiagnosisStatusChart = {
+    name: string;
+    value: number;
+}
+
+export type DiseaseVsHealthyChart = {
+    name: string;
+    value: number;
+}
+
+export type CholesteatomaStagesChart = {
+    stage: string;
+    count: number;
+}
+
+export type ConfidenceScoreDistributionChart = {
+    scoreRange: string;
+    count: number;
+}
