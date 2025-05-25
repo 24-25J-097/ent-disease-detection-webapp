f131b83bf66e1b9212df45a2394c052693c8efa6
Fonseka M.M.N.H <it21156410@my.sliit.lk>
2025-05-26 02:10:10 +0530

    [commercialization] usage reports (Not complete)

---
+++ src/models/RequestLog.ts
@@ -0,0 +1,69 @@
+import {User} from './User';
+import {Package} from './Package';
+
+
+export interface RequestLogResponse {
+    dateRange: {
+        startDate: string;
+        endDate: string;
+    },
+    totalRequests: number;
+    dailySummary: {
+        date: string;
+        count: number;
+    }[];
+    data: RequestLog[];
+}
+
+export interface RequestLog {
+    _id: string;
+    user?: User;
+    endpoint: string;
+    method: string;
+    statusCode: number;
+    responseTime: number;
+    userAgent: string;
+    ip: string;
+    timestamp: string;
+    createdAt: string | null;
+    updatedAt: string | null;
+}
+
+
+export interface UsageByUser {
+    _id: string;
+    count: number;
+    endpoints: string[];
+    userDetails: {
+        _id: string;
+        name: string;
+        email: string;
+        role: string;
+    }
+}
+
+export interface UsageByRole {
+    _id: string;
+    count: number;
+    endpoints: string[];
+    roleDetails: {
+        _id: string;
+        role: string;
+    }
+}
+
+export interface UsageByPackage {
+    _id: string;
+    count: number;
+    endpoints: string[];
+    packageDetails: {
+        _id: string;
+        name: string;
+    }
+}
+
+export interface PackagePurchase {
+    date: string;
+    packageName: string;
+    count: number;
+}
